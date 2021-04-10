d3edfe2d164df98eef13c111f20362449b934837
Naoki Maruyama <m.naoki85@gmail.com>
2021-04-10 19:07:19 +0900

    craate sample page

--- laravel_practice/laravelapp/routes/web.php
+++ laravel_practice/laravelapp/routes/web.php
@@ -16,3 +16,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+$html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
+<style>
+body { font-size: 16px; color:#999; }
+h1 { font-size: 100pt; text-align: right; color: #eee;
+    margin: -40px 0px -50px 0px; }
+</style>
+</head>
+<body>
+    <h1>Hello</h1>
+    <p>This is a sample page.</p>
+    <p>I will do my best to learn Laravel</p>
+</body>
+</html>
+EOF;
+
+Route::get('hello',function () use($html){
+    return $html;
+});
